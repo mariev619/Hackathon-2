5fc636b4a93ae9a84adb58601d786857a9b194c4
Marie Vendran <marie.vendran@gmail.com>
2019-12-19 13:05:39 +0100

    second commit

---
+++ Hachathon/src/main/java/com/wildcodeschool/Hachathon/HachathonApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.Hachathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HachathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HachathonApplication.class, args);
+	}
+
+}
---
+++ Hachathon/src/test/java/com/wildcodeschool/Hachathon/HachathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.Hachathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HachathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
